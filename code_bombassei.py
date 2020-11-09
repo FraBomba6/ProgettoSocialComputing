9ee7dd44f077d15bdf0ddca387b1ed8a5b207232
bomb697 <50354770+bomb697@users.noreply.github.com>
2020-11-09 16:49:43 +0100

    Aggiunto lo script per i random picker

--- code_bombassei.py
+++ code_bombassei.py
@@ -1,4 +1,6 @@
 # %%
+import random
+
 from config import *
 import ProgettoSocialComputing.serializer as se
 
@@ -20,7 +22,7 @@ for user in users:
         user_friends.append(found_friend)
 
     print(f"Found {len(user_friends)} followers for @{user}")
-    serializer = se.Serializer('ProgettoSocialComputing/followers')
+    serializer = se.Serializer('./followers')
     serializer.serialize_json(f"{user}_followers.json", user_friends)
 
 # %%
@@ -41,5 +43,41 @@ for user in users:
         user_friends.append(found_friend)
 
     print(f"@{user} follows {len(user_friends)} users")
-    serializer = se.Serializer('ProgettoSocialComputing/following')
+    serializer = se.Serializer('./following')
     serializer.serialize_json(f"{user}_following.json", user_friends)
+
+# %%
+users = ["mizzaro", "damiano10", "Miccighel_", "eglu81", "KevinRoitero"]
+serializer = se.Serializer('./followers')
+for user in users:
+    json = serializer.read_json(f"{user}_followers.json")
+    for count in range(0, 5):
+        random_follower = random.choice(json)["screen_name"]
+        random_follower_followers = []
+        for item in tweepy.Cursor(
+                api.followers,
+                screen_name=random_follower,
+                skip_status=True,
+                include_user_entities=False
+        ).items(10):
+            found_follower = item._json
+            random_follower_followers.append(found_follower)
+        print(f"Found {len(random_follower_followers)} followers for @{random_follower}")
+        serializer = se.Serializer('./following')
+        serializer.serialize_json(f"{random_follower}_follower.json", random_follower_followers)
+
+    json = serializer.read_json(f"{user}_following.json")
+    for count in range(0, 5):
+        random_friend = random.choice(json)["screen_name"]
+        random_friend_friends = []
+        for item in tweepy.Cursor(
+                api.following,
+                screen_name=random_friend,
+                skip_status=True,
+                include_user_entities=False
+        ).items(10):
+            found_friend = item._json
+            random_friend_friends.append(found_friend)
+        print(f"Found {len(random_friend_friends)} followers for @{random_friend}")
+        serializer = se.Serializer('./following')
+        serializer.serialize_json(f"{random_friend}_follower.json", random_friend_friends)
