e5ef89487a20f84b118cfa6e8359102c3e2c67bb
bomb697 <50354770+bomb697@users.noreply.github.com>
2020-11-09 17:47:26 +0100

    Update del randomizer

--- code_bombassei.py
+++ code_bombassei.py
@@ -48,8 +48,8 @@ for user in users:
 
 # %%
 users = ["mizzaro", "damiano10", "Miccighel_", "eglu81", "KevinRoitero"]
-serializer = se.Serializer('./followers')
 for user in users:
+    serializer = se.Serializer(f'data/{user}')
     json = serializer.read_json(f"{user}_followers.json")
     for count in range(0, 5):
         random_follower = random.choice(json)["screen_name"]
@@ -63,7 +63,6 @@ for user in users:
             found_follower = item._json
             random_follower_followers.append(found_follower)
         print(f"Found {len(random_follower_followers)} followers for @{random_follower}")
-        serializer = se.Serializer('./following')
         serializer.serialize_json(f"random_{random_follower}_follower.json", random_follower_followers)
 
     json = serializer.read_json(f"{user}_following.json")
@@ -71,7 +70,7 @@ for user in users:
         random_friend = random.choice(json)["screen_name"]
         random_friend_friends = []
         for item in tweepy.Cursor(
-                api.following,
+                api.friends,
                 screen_name=random_friend,
                 skip_status=True,
                 include_user_entities=False
@@ -79,5 +78,4 @@ for user in users:
             found_friend = item._json
             random_friend_friends.append(found_friend)
         print(f"@{random_friend} follows {len(random_friend_friends)} users")
-        serializer = se.Serializer('./following')
         serializer.serialize_json(f"random_{random_friend}_following.json", random_friend_friends)
